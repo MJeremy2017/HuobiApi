43665784177165dace8e3baa7cdd0d9a032608d2
jeremy.zhang <zhangyue9306@gmail.com>
2020-01-29 20:52:31 +0800

    add setup pkg

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="autoetl",
-    version="0.0.15",
-    author="Jeremy.Zhang, Randen.Rosete",
-    author_email="jeremy.zhang@grabtaxi.com",
-    description="automate etl tool",
+    name="huobiApi",
+    version="0.0.1",
+    author="Jeremy.Zhang",
+    author_email="zhangyue93006@gmail.com",
+    description="helper functions of huobi api call",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.myteksi.net/data-science/food/autoetl",
+    url="https://github.com/MJeremy2017/HuobiApi",
     packages=setuptools.find_packages(),
     install_requires=open('requirements.txt').readlines(),
     include_package_data=True,
@@ -20,5 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
