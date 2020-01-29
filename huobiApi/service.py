1afcb61982918e023469b30422b670721da4f7f6
jeremy.zhang <zhangyue9306@gmail.com>
2020-01-29 21:09:34 +0800

    restruct to 0.03

--- huobiApi/service.py
+++ huobiApi/service.py
@@ -8,7 +8,7 @@ import urllib.parse
 import urllib.request
 import requests
 import pandas as pd
-from huobiApi.model.config import TRADE_URL, MARKET_URL
+from huobiApi.config import TRADE_URL, MARKET_URL
 
 
 class HuobiSVC:
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="huobiApi",
-    version="0.0.1",
+    version="0.0.2",
     author="Jeremy.Zhang",
     author_email="zhangyue93006@gmail.com",
     description="helper functions of huobi api call",
